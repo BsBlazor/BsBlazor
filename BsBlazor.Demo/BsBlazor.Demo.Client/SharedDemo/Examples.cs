02dddfea26dccb2e1708798272d5812005480f37
Guilherme Meinlschmiedt Abdo <guilherme.abdo@codeblox.com.br>
2024-05-09 17:09:50 -0300

    ✨ code snippets in demo app

---
+++ BsBlazor.Demo/BsBlazor.Demo.Client/SharedDemo/Examples.cs
@@ -0,0 +1,25 @@
+﻿namespace BsBlazor;
+public static class Examples 
+{
+	public static readonly IReadOnlyDictionary<string, string> Contents = new Dictionary<string, string>(){
+		{
+			"ModalStaticExample",
+"""
+<BsModal Class="position-static d-block" Fade="false">
+    <ModalHeader>
+        <BsModalTitle>Modal title</BsModalTitle>
+        <BsModalCloseButton />
+    </ModalHeader>
+    <ChildContent>
+        Modal body text goes here.
+    </ChildContent>
+    <ModalFooter>
+        <BsButton Variant="BsButtonVariant.Secondary" Dismiss="BsButtonDismiss.Modal">Close</BsButton>
+        <BsButton Variant="BsButtonVariant.Primary">Save Changes</BsButton>
+    </ModalFooter>
+</BsModal>
+
+"""
+		}
+	};
+}
---
+++ BsBlazor/BsModalFullscreen.cs
@@ -0,0 +1,12 @@
+﻿namespace BsBlazor;
+
+public enum BsModalFullscreen
+{
+    Never = 0,
+    Always = 1,
+    BelowSmall = 2,
+    BelowMedium = 3,
+    BelowLarge = 4,
+    BelowExtraLarge = 5,
+    BelowExtraExtraLarge = 6,
+}
