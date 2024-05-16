c110ac5b8078d8970953f1148e24ffc9bfd72038
Emerson Trindade (MensoDev) <emerson.trindade@codeblox.com.br>
2024-05-16 17:22:45 -0300

    🏗️ progride componente para facilitar uso código de exemplo |

--- BsBlazor/Helpers/BsCssBuilderUtilitiesBorder.cs
+++ BsBlazor/Helpers/BsCssBuilderUtilitiesBorder.cs
@@ -4,10 +4,43 @@ public partial class BsCssBuilder
 {
     // Border
     public BsCssBuilder Border => AddClass("border");
+    
     public BsCssBuilder BorderTop => AddClass("border-top");
+    public BsCssBuilder BorderTop0 => AddClass("border-top-0");
+    public BsCssBuilder BorderTop1 => AddClass("border-top-1");
+    public BsCssBuilder BorderTop2 => AddClass("border-top-2");
+    public BsCssBuilder BorderTop3 => AddClass("border-top-3");
+    public BsCssBuilder BorderTop4 => AddClass("border-top-4");
+    public BsCssBuilder BorderTop5 => AddClass("border-top-5");
+    
     public BsCssBuilder BorderEnd => AddClass("border-end");
+    public BsCssBuilder BorderEnd0 => AddClass("border-end-0");
+    public BsCssBuilder BorderEnd1 => AddClass("border-end-1");
+    public BsCssBuilder BorderEnd2 => AddClass("border-end-2");
+    public BsCssBuilder BorderEnd3 => AddClass("border-end-3");
+    public BsCssBuilder BorderEnd4 => AddClass("border-end-4");
+    public BsCssBuilder BorderEnd5 => AddClass("border-end-5");
+    public BsCssBuilder BorderEndPill => AddClass("border-end-pill");
+    public BsCssBuilder BorderEndCircle => AddClass("border-end-circle");
+    
     public BsCssBuilder BorderBottom => AddClass("border-bottom");
+    public BsCssBuilder BorderBottom0 => AddClass("border-bottom-0");
+    public BsCssBuilder BorderBottom1 => AddClass("border-bottom-1");
+    public BsCssBuilder BorderBottom2 => AddClass("border-bottom-2");
+    public BsCssBuilder BorderBottom3 => AddClass("border-bottom-3");
+    public BsCssBuilder BorderBottom4 => AddClass("border-bottom-4");
+    public BsCssBuilder BorderBottom5 => AddClass("border-bottom-5");
+    
+    
     public BsCssBuilder BorderStart => AddClass("border-start");
+    public BsCssBuilder BorderStart0 => AddClass("border-start-0");
+    public BsCssBuilder BorderStart1 => AddClass("border-start-1");
+    public BsCssBuilder BorderStart2 => AddClass("border-start-2");
+    public BsCssBuilder BorderStart3 => AddClass("border-start-3");
+    public BsCssBuilder BorderStart4 => AddClass("border-start-4");
+    public BsCssBuilder BorderStart5 => AddClass("border-start-5");
+    public BsCssBuilder BorderStartPill => AddClass("border-start-pill");
+    public BsCssBuilder BorderStartCircle => AddClass("border-start-circle");
     
     // Border Color
     public BsCssBuilder BorderPrimary => AddClass("border-primary");
@@ -28,5 +61,50 @@ public partial class BsCssBuilder
     public BsCssBuilder BorderDarkSubtle => AddClass("border-dark-subtle");
     public BsCssBuilder BorderBlack => AddClass("border-black");
     public BsCssBuilder BorderWhite => AddClass("border-white");
+    
+    // Rounded
+    public BsCssBuilder Rounded => AddClass("rounded");
+    public BsCssBuilder Rounded0 => AddClass("rounded-0");
+    public BsCssBuilder Rounded1 => AddClass("rounded-1");
+    public BsCssBuilder Rounded2 => AddClass("rounded-2");
+    public BsCssBuilder Rounded3 => AddClass("rounded-3");
+    public BsCssBuilder Rounded4 => AddClass("rounded-4");
+    public BsCssBuilder Rounded5 => AddClass("rounded-5");
+    
+    public BsCssBuilder RoundedPill => AddClass("rounded-pill");
+    public BsCssBuilder RoundedCircle => AddClass("rounded-circle");
+    
+    public BsCssBuilder RoundedTop => AddClass("rounded-top");
+    public BsCssBuilder RoundedTop0 => AddClass("rounded-top-0");
+    public BsCssBuilder RoundedTop1 => AddClass("rounded-top-1");
+    public BsCssBuilder RoundedTop2 => AddClass("rounded-top-2");
+    public BsCssBuilder RoundedTop3 => AddClass("rounded-top-3");
+    public BsCssBuilder RoundedTop4 => AddClass("rounded-top-4");
+    public BsCssBuilder RoundedTop5 => AddClass("rounded-top-5");
+    
+    public BsCssBuilder RoundedEnd => AddClass("rounded-end");
+    public BsCssBuilder RoundedEnd0 => AddClass("rounded-end-0");
+    public BsCssBuilder RoundedEnd1 => AddClass("rounded-end-1");
+    public BsCssBuilder RoundedEnd2 => AddClass("rounded-end-2");
+    public BsCssBuilder RoundedEnd3 => AddClass("rounded-end-3");
+    public BsCssBuilder RoundedEnd4 => AddClass("rounded-end-4");
+    public BsCssBuilder RoundedEnd5 => AddClass("rounded-end-5");
+    
+    public BsCssBuilder RoundedBottom => AddClass("rounded-bottom");
+    public BsCssBuilder RoundedBottom0 => AddClass("rounded-bottom-0");
+    public BsCssBuilder RoundedBottom1 => AddClass("rounded-bottom-1");
+    public BsCssBuilder RoundedBottom2 => AddClass("rounded-bottom-2");
+    public BsCssBuilder RoundedBottom3 => AddClass("rounded-bottom-3");
+    public BsCssBuilder RoundedBottom4 => AddClass("rounded-bottom-4");
+    public BsCssBuilder RoundedBottom5 => AddClass("rounded-bottom-5");
+    
+    public BsCssBuilder RoundedStart => AddClass("rounded-start");
+    public BsCssBuilder RoundedStart0 => AddClass("rounded-start-0");
+    public BsCssBuilder RoundedStart1 => AddClass("rounded-start-1");
+    public BsCssBuilder RoundedStart2 => AddClass("rounded-start-2");
+    public BsCssBuilder RoundedStart3 => AddClass("rounded-start-3");
+    public BsCssBuilder RoundedStart4 => AddClass("rounded-start-4");
+    public BsCssBuilder RoundedStart5 => AddClass("rounded-start-5");
+    
     
 }
