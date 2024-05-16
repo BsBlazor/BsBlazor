ae98d666f8ca1600d2592a92cd82873cb4113cac
Emerson Trindade (MensoDev) <emerson.trindade@codeblox.com.br>
2024-05-16 16:49:53 -0300

    ⭐ add section 'Background' in BsCssBuilder

---
+++ BsBlazor/Helpers/BsCssBuilderUtilitiesBackground.cs
@@ -0,0 +1,28 @@
+﻿namespace BsBlazor;
+
+public partial class BsCssBuilder
+{
+    // Background
+    public BsCssBuilder BackgroundPrimary => AddClass("bg-primary");
+    public BsCssBuilder BackgroundPrimarySubtle => AddClass("bg-primary-subtle");
+    public BsCssBuilder BackgroundSecondary => AddClass("bg-secondary");
+    public BsCssBuilder BackgroundSecondarySubtle => AddClass("bg-secondary-subtle");
+    public BsCssBuilder BackgroundSuccess => AddClass("bg-success-subtle");
+    public BsCssBuilder BackgroundSuccessSubtle => AddClass("bg-success-subtle");
+    public BsCssBuilder BackgroundDanger => AddClass("bg-danger");
+    public BsCssBuilder BackgroundDangerSubtle => AddClass("bg-danger-subtle");
+    public BsCssBuilder BackgroundWarning => AddClass("bg-warning");
+    public BsCssBuilder BackgroundWarningSubtle => AddClass("bg-warning-subtle");
+    public BsCssBuilder BackgroundInfo => AddClass("bg-info");
+    public BsCssBuilder BackgroundInfoSubtle => AddClass("bg-info-subtle");
+    public BsCssBuilder BackgroundLight => AddClass("bg-light");
+    public BsCssBuilder BackgroundLightSubtle => AddClass("bg-light-subtle");
+    public BsCssBuilder BackgroundDark => AddClass("bg-dark");
+    public BsCssBuilder BackgroundDarkSubtle => AddClass("bg-dark-subtle");
+    public BsCssBuilder BackgroundBodySecondary => AddClass("bg-body-secondary");
+    public BsCssBuilder BackgroundBodyTertiary => AddClass("bg-body-tertiary");
+    public BsCssBuilder BackgroundBody => AddClass("bg-body");
+    public BsCssBuilder BackgroundBlack => AddClass("bg-black");
+    public BsCssBuilder BackgroundWhite => AddClass("bg-white");
+    public BsCssBuilder BackgroundTransparent => AddClass("bg-transparent");
+}
