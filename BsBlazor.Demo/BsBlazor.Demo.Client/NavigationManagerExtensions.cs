c68a5ff03965c9282e93f88a0586709a2ec2aaed
Guilherme Meinlschmiedt Abdo <guilherme.abdo@codeblox.com.br>
2024-05-17 00:14:14 -0300

    redesigning demo application

---
+++ BsBlazor.Demo/BsBlazor.Demo.Client/NavigationManagerExtensions.cs
@@ -0,0 +1,14 @@
+﻿//using Microsoft.AspNetCore.Components;
+//using Microsoft.AspNetCore.Components.Web;
+
+//namespace BsBlazor.Demo.Client;
+
+//public static class NavigationManagerExtensions
+//{
+//    public static IComponentRenderMode RenderMode(this NavigationManager navigationManager)
+//    {
+//        return new Static
+//        return new InteractiveWebAssemblyRenderMode();
+//        return Microsoft.AspNetCore.Components.Web.RenderMode.
+//    }
+//}
---
+++ BsBlazor/Helpers/BsCssBuilderUtilitiesOverflow.cs
@@ -0,0 +1,19 @@
+﻿namespace BsBlazor;
+public partial class BsCssBuilder
+{
+    // Overflow
+    public BsCssBuilder OverflowAuto => AddClass("overflow-auto");
+    public BsCssBuilder OverflowHidden => AddClass("overflow-hidden");
+    public BsCssBuilder OverflowVisible => AddClass("overflow-visible");
+    public BsCssBuilder OverflowScroll => AddClass("overflow-scroll");
+
+    public BsCssBuilder OverflowXAuto => AddClass("overflow-x-auto");
+    public BsCssBuilder OverflowXHidden => AddClass("overflow-x-hidden");
+    public BsCssBuilder OverflowXVisible => AddClass("overflow-x-visible");
+    public BsCssBuilder OverflowXScroll => AddClass("overflow-x-scroll");
+
+    public BsCssBuilder OverflowYAuto => AddClass("overflow-y-auto");
+    public BsCssBuilder OverflowYHidden => AddClass("overflow-y-hidden");
+    public BsCssBuilder OverflowYVisible => AddClass("overflow-y-visible");
+    public BsCssBuilder OverflowYScroll => AddClass("overflow-y-scroll");
+}
