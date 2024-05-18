07894b97e57ac964fe3d87696b4bf1d5e12df285
Guilherme Meinlschmiedt Abdo <guilherme.abdo@codeblox.com.br>
2024-05-17 21:27:49 -0300

    demo - create page base to infer RenderMode form route

---
+++ BsBlazor.Demo/BsBlazor.Demo.Client/Pages/DynamicRenderModePage.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace BsBlazor.Demo.Client.Pages;
+
+public class DynamicRenderModePage : ComponentBase
+{
+    [Parameter]
+    public string? Mode { get; set; }
+    public IComponentRenderMode? RenderModeFromRoute => Mode switch
+    {
+        "server" => new InteractiveServerRenderMode(),
+        "wasm" => new InteractiveWebAssemblyRenderMode(),
+        _ => null
+    };
+}
