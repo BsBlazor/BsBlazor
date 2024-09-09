5a581443d20b44f217b2ecda5503318b586fc004
Guilherme Meinlschmiedt Abdo <guilherme.abdo@codeblox.com.br>
2024-09-09 16:23:49 -0300

    🚧 building form components

---
+++ BsBlazor/Helpers/BsCssBuilderHelpersForms.cs
@@ -0,0 +1,18 @@
+﻿namespace BsBlazor;
+public partial class BsCssBuilder
+{
+    /// <summary>
+    /// form-floating
+    /// </summary>
+    public BsCssBuilder FormFloating => AddClass("form-floating");
+
+    /// <summary>
+    /// form-label
+    /// </summary>
+    public BsCssBuilder FormLabel => AddClass("form-label");
+
+    /// <summary>
+    /// form-text
+    /// </summary>
+    public BsCssBuilder FormText => AddClass("form-text");
+}
