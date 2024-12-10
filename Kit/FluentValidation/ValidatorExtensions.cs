ecbaef3b3d2d5bbe30db92a3e90120a9f55e887d
Guilherme Meinlschmiedt Abdo <guilherme.abdo@codeblox.com.br>
2024-12-10 15:32:37 -0300

    🚨back to the initial idea to track required by analyzing the val descriptor

---
+++ Kit/FluentValidation/ValidatorExtensions.cs
@@ -0,0 +1,92 @@
+﻿using FluentValidation.Validators;
+using System.Collections;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace FluentValidation;
+public static class ValidatorExtensions
+{
+    public static bool IsRequired<T, TProperty>(this IValidator validator, T rootInstance, Expression<Func<TProperty>> valueExpression)
+    {
+        var member = valueExpression.Body as MemberExpression ?? (valueExpression.Body as UnaryExpression)?.Operand as MemberExpression;
+        if (member == null)
+        {
+            throw new ArgumentException("Expression must be a member expression");
+        }
+        var targetModel = Expression.Lambda(member.Expression!).Compile().DynamicInvoke();
+        var fieldName = member.Member.Name;
+        return validator.IsRequired(rootInstance, targetModel ?? rootInstance!, fieldName);
+    }
+
+    public static bool IsRequired<T>(
+        this IValidator validator,
+        T rootInstance,
+        object targetInstance,
+        string fieldName)
+    {
+        var descriptor = validator.CreateDescriptor();
+        return IsInlineRequired(descriptor, rootInstance, targetInstance, fieldName)
+            || IsChildAdaptorRequired(descriptor, rootInstance, targetInstance, fieldName);
+    }
+
+    private static bool IsChildAdaptorRequired(IValidatorDescriptor descriptor, object rootInstance, object targetInstance, string fieldName)
+    {
+        var rulesWithChildAdaptors = descriptor.Rules.Where(r => r.Components.Any(c => c.Validator.Name == nameof(ChildValidatorAdaptor<object, object>))).ToArray();
+        foreach (var ruleWithChildAdaptor in rulesWithChildAdaptors)
+        {
+            var childInstance = ruleWithChildAdaptor.Expression.Compile().DynamicInvoke(rootInstance);
+            if (childInstance is null) { continue; }
+            var adaptorComponent = ruleWithChildAdaptor.Components.FirstOrDefault(c => c.Validator.Name == nameof(ChildValidatorAdaptor<object, object>));
+            var adaptor = adaptorComponent.Validator as IChildValidatorAdaptor;
+            var innerValidator = adaptor.GetType().GetField("_validator", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(adaptor) as IValidator;
+            var innerValidatorProvider = adaptor.GetType().GetField("_validatorProvider", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(adaptor) as Delegate;
+            if (ruleWithChildAdaptor.GetType().Name == "CollectionPropertyRule`2")
+            {
+                var items = childInstance as IEnumerable;
+                foreach (var item in items)
+                {
+                    var validator = innerValidator ?? innerValidatorProvider.DynamicInvoke(
+                        Activator.CreateInstance(typeof(ValidationContext<>).MakeGenericType(rootInstance.GetType()), rootInstance),
+                        item) as IValidator;
+                    var childValidatorDescriptor = validator.CreateDescriptor();
+                    var isRequired = IsInlineRequired(childValidatorDescriptor, item, targetInstance, fieldName) || IsChildAdaptorRequired(childValidatorDescriptor, items, targetInstance, fieldName);
+                    if(isRequired) { return true; }
+                }
+            }
+            else // RuleFor(x => x.Child).Etc
+            {
+                var validator = innerValidator ?? innerValidatorProvider.DynamicInvoke(
+                    Activator.CreateInstance(typeof(ValidationContext<>).MakeGenericType(rootInstance.GetType()), rootInstance),
+                    childInstance) as IValidator;
+                var childValidatorDescriptor = validator.CreateDescriptor();
+                var isRequired = IsInlineRequired(childValidatorDescriptor, childInstance, targetInstance, fieldName) || IsChildAdaptorRequired(childValidatorDescriptor, childInstance, targetInstance, fieldName);
+                if (isRequired) { return true; }
+            }
+        }
+        return false;
+    }
+
+    private static bool IsInlineRequired(IValidatorDescriptor descriptor, object rootInstance, object targetInstance, string fieldName)
+    {
+        var ruleWithRequiredValidator = descriptor.Rules
+                                           .Where(r => r.PropertyName.Split('.').Last() == fieldName)
+                                           .Where(r => r.Components.Any(c => c.Validator is INotEmptyValidator or INotNullValidator))
+                                           .FirstOrDefault(r =>
+                                           {
+                                               var memberExpression = r.Expression.Body as MemberExpression ?? (r.Expression.Body as UnaryExpression)?.Operand as MemberExpression;
+                                               var compiledTarget = Expression.Lambda(memberExpression.Expression!, r.Expression.Parameters).Compile().DynamicInvoke(rootInstance);
+                                               return compiledTarget == targetInstance;
+                                           });
+        if (ruleWithRequiredValidator == null) { return false; }
+        var requiredComponent = ruleWithRequiredValidator.Components.FirstOrDefault(c => c.Validator is INotEmptyValidator or INotNullValidator);
+        if (requiredComponent.HasCondition)
+        {
+            var conditionField = requiredComponent.GetType().GetField("_condition", BindingFlags.NonPublic | BindingFlags.Instance);
+            var condition = conditionField?.GetValue(requiredComponent) as Delegate;
+            var context = Activator.CreateInstance(typeof(ValidationContext<>).MakeGenericType(rootInstance.GetType()), rootInstance);
+            if (!((bool)condition.DynamicInvoke(context))) { return false; }
+        }
+
+        return true;
+    }
+}
--- Tests/Plus/FluentValidationExtensionsTests.cs
+++ Tests/Plus/FluentValidationExtensionsTests.cs
@@ -1,156 +1,181 @@
-﻿//using FluentAssertions;
-//using FluentValidation;
-//using FluentValidation.TestHelper;
-//using System.Linq.Expressions;
+﻿using FluentAssertions;
+using FluentValidation;
+using FluentValidation.TestHelper;
+using System.Linq.Expressions;
 
-//namespace BsBlazor.Tests.Plus;
-//public class FluentValidationExtensionsTests
-//{
-//    [Fact]
-//    public void MemberTest()
-//    {
-//        var validator = new ModelValidator();
-//        var model = new Model();
-//        validator.IsRequired(model, model, "Name").Should().BeTrue();
-//    }
+namespace BsBlazor.Tests.Plus;
 
-//    [Fact]
-//    public void MemberExpressionTest()
-//    {
-//        var validator = new ModelValidator();
-//        var model = new Model();
-//        Expression<Func<string>> valueExpression = () => model.Name;
-//        validator.IsRequired(model, valueExpression).Should().BeTrue();
-//    }
+public class FluentValidationExtensionsTests
+{
+    [Fact]
+    public void MemberTest()
+    {
+        var validator = new ModelValidator();
+        var model = new Model();
+        validator.IsRequired(model, model, "Name").Should().BeTrue();
+    }
 
-//    [Fact]
-//    public void ChildMemberTest()
-//    {
-//        var validator = new ModelValidator();
-//        var model = new Model();
-//        Expression<Func<string>> valueExpression = () => model.Child.ChildName;
-//        validator.IsRequired(model, valueExpression).Should().BeTrue();
-//    }
+    [Fact]
+    public void MemberExpressionTest()
+    {
+        var validator = new ModelValidator();
+        var model = new Model();
+        Expression<Func<string>> valueExpression = () => model.Name;
+        validator.IsRequired(model, valueExpression).Should().BeTrue();
+    }
 
-//    [Fact]
-//    public void ChildIndirectExpressionMemberTest()
-//    {
-//        var validator = new ModelValidator();
-//        var model = new Model();
-//        var child = model.Child;
-//        Expression<Func<string>> valueExpression = () => child.ChildName;
-//        validator.IsRequired(model, valueExpression).Should().BeTrue();
-//    }
+    [Fact]
+    public void ChildMemberTest()
+    {
+        var validator = new ModelValidator();
+        var model = new Model();
+        Expression<Func<string>> valueExpression = () => model.Child.ChildName;
+        validator.IsRequired(model, valueExpression).Should().BeTrue();
+    }
 
-//    [Fact]
-//    public void SelfTest()
-//    {
-//        var validator = new SelfValidator();
-//        validator.IsRequired(this, this, "Name").Should().BeTrue();
-//    }
+    [Fact]
+    public void ChildIndirectExpressionMemberTest()
+    {
+        var validator = new ModelValidator();
+        var model = new Model();
+        var child = model.Child;
+        Expression<Func<string>> valueExpression = () => child.ChildName;
+        validator.IsRequired(model, valueExpression).Should().BeTrue();
+    }
 
-//    [Fact]
-//    public void SelfExpressionTest()
-//    {
-//        var validator = new SelfValidator();
-//        Expression<Func<string>> valueExpression = () => Name;
-//        validator.IsRequired(this, valueExpression).Should().BeTrue();
-//    }
+    [Fact]
+    public void SelfTest()
+    {
+        var validator = new SelfValidator();
+        validator.IsRequired(this, this, "Name").Should().BeTrue();
+    }
 
-//    [Fact]
-//    public void ComplexChildrenValidatorTest()
-//    {
-//        var validator = new ModelValidator();
-//        var model = new Model();
-//        model.Children.Add(new Child());
-//        model.Children.Add(new Child());
-//        validator.IsRequired(model, model.Children[0], "ChildName").Should().BeFalse();
-//        validator.IsRequired(model, model.Children[1], "ChildName").Should().BeTrue();
-//    }
+    [Fact]
+    public void SelfExpressionTest()
+    {
+        var validator = new SelfValidator();
+        Expression<Func<string>> valueExpression = () => Name;
+        validator.IsRequired(this, valueExpression).Should().BeTrue();
+    }
 
-//    [Fact]
-//    public void InlineComplexTest()
-//    {
-//        var validator = new ModelInlineValidator();
-//        var model = new Model();
-//        validator.IsRequired(model, () => model.Child.ChildName).Should().BeTrue();
-//    }
+    [Fact]
+    public void ComplexChildrenValidatorTest()
+    {
+        var validator = new RuleForEach_SetValidator_Validator();
+        var model = new Model();
+        model.Children.Add(new Child());
+        model.Children.Add(new Child());
+        validator.IsRequired(model, model.Children[1], "ChildName").Should().BeTrue();
+        validator.IsRequired(model, model.Children[0], "ChildName").Should().BeFalse();
+    }
+    internal class RuleForEach_SetValidator_Validator : AbstractValidator<Model>
+    {
+        public RuleForEach_SetValidator_Validator()
+        {
+            RuleForEach(x => x.Children).SetValidator(m => new ChildByIndexValidator(m));
+        }
+    }
 
-//    [Fact]
-//    public void NotCurrentlySupported_InlineComplexCollectionTest()
-//    {
-//        var validator = new ModelInlineCollectionValidator();
-//        var model = new Model();
-//        model.Children.Add(new Child());
-//        model.Children.Add(new Child());
-//        validator.TestValidate(model).ShouldHaveValidationErrorFor(model => model.Children[1].ChildName)
-//            .WithErrorCode("NotEmptyValidator");
-//        validator.IsRequired(model, model.Children[1], "ChildName").Should().BeFalse();
+    [Fact]
+    public void InlineComplexTest()
+    {
+        var validator = new ModelInlineValidator();
+        var model = new Model();
+        validator.IsRequired(model, () => model.Child.ChildName).Should().BeTrue();
+    }
+    internal class ModelInlineValidator : AbstractValidator<Model>
+    {
+        public ModelInlineValidator()
+        {
+            RuleFor(x => x.Child.ChildName).NotEmpty();
+        }
+    }
 
-//    }
+    [Fact]
+    public void InlineComplexCollectionTest()
+    {
+        var validator = new ModelInlineCollectionValidator();
+        var model = new Model();
+        model.Children.Add(new Child());
+        model.Children.Add(new Child());
+        validator.TestValidate(model).ShouldHaveValidationErrorFor(model => model.Children[1].ChildName)
+            .WithErrorCode("NotEmptyValidator");
+        // 
+        validator.IsRequired(model, model.Children[0], "ChildName").Should().BeFalse();
+        validator.IsRequired(model, model.Children[1], "ChildName").Should().BeTrue();
 
-//    public class ModelInlineValidator: AbstractValidator<Model>
-//    {
-//        public ModelInlineValidator()
-//        {
-//            RuleFor(x => x.Child.ChildName).NotEmptyTrackRequired();
-//        }
-//    }
+    }
+    internal class ModelInlineCollectionValidator : AbstractValidator<Model>
+    {
+        public ModelInlineCollectionValidator()
+        {
+            RuleFor(x => x.Children[1].ChildName).NotEmpty();
+        }
+    }
 
-//    public class ModelInlineCollectionValidator : AbstractValidator<Model>
-//    {
-//        public ModelInlineCollectionValidator()
-//        {
-//            RuleFor(x => x.Children[1].ChildName).NotEmptyTrackRequired();
-//        }
-//    }
+    public string Name { get; set; } = string.Empty;
+    internal class SelfValidator : AbstractValidator<FluentValidationExtensionsTests> 
+    {
+        public SelfValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty();
+        }
+    }
 
-//    public string Name { get; set; } = string.Empty;
-//    public class SelfValidator : AbstractValidator<FluentValidationExtensionsTests>
-//    {
-//        public SelfValidator()
-//        {
-//            RuleFor(x => x.Name).NotEmptyTrackRequired();
-//        }
-//    }
+    internal class ModelValidator : AbstractValidator<Model> 
+    {
+        public ModelValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().When(x => true);
+            RuleFor(x => x.Child).SetValidator(new ChildValidator());
+            RuleForEach(x => x.Children).SetValidator(m => new ChildByIndexValidator(m));
+        }
+    }
 
-//    public class ModelValidator : AbstractValidator<Model> 
-//    {
-//        public ModelValidator()
-//        {
-//            RuleFor(x => x.Name).NotEmptyTrackRequired();
-//            RuleFor(x => x.Child).SetValidator(new ChildValidator());
-//            RuleForEach(x => x.Children).SetValidator(m => new ChildByIndexValidator(m));
-//        }
-//    }
+    public class Model
+    {
+        public string Name { get; set; } = string.Empty;
+        public Child Child { get; set; } = new Child();
+        public List<Child> Children { get; set; } = new();
+    }
 
-//    public class Model
-//    {
-//        public string Name { get; set; } = string.Empty;
-//        public Child Child { get; set; } = new Child();
-//        public List<Child> Children { get; set; } = new();
-//    }
+    public class Child
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string ChildName { get; set; } = string.Empty;
+    }
 
-//    public class Child
-//    {
-//        public Guid Id { get; set; } = Guid.NewGuid();
-//        public string ChildName { get; set; } = string.Empty;
-//    }
+    internal class ChildByIndexValidator : AbstractValidator<Child>
+    {
+        public ChildByIndexValidator(Model model)
+        {
+            RuleFor(x => x.ChildName).NotEmpty()
+                                     .When(x => model.Children.LastOrDefault() == x);
+        }
+    }
 
-//    public class ChildByIndexValidator: AbstractValidator<Child>
-//    {
-//        public ChildByIndexValidator(Model model)
-//        {
-//            RuleFor(x => x.ChildName).NotEmptyTrackRequired()
-//                                     .When(x => model.Children.LastOrDefault() == x);
-//        }
-//    }
+    internal class ChildValidator : AbstractValidator<Child>
+    {
+        public ChildValidator()
+        {
+            RuleFor(x => x.ChildName).NotEmpty().When(c => true);
+        }
+    }
 
-//    public class ChildValidator : AbstractValidator<Child> 
-//    {
-//        public ChildValidator()
-//        {
-//            RuleFor(x => x.ChildName).NotEmptyTrackRequired().When(c => true);
-//        }
-//    }
-//}
+    [Fact]
+    public void InlineChildComplexTest()
+    {
+        var validator = new InlineChildComplexValidator();
+        var model = new Model();
+        validator.IsRequired(model, model.Child, "ChildName").Should().BeTrue();
+    }
+
+    public class InlineChildComplexValidator : AbstractValidator<Model>
+    {
+        public InlineChildComplexValidator()
+        {
+            RuleFor(x => x.Child).ChildRules(c => c.RuleFor(c => c.ChildName).NotEmpty());
+        }
+    }
+
+}
