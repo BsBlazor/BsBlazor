92f574f8b67807590311bf73c62ab4b4cf980527
Guilherme Meinlschmiedt Abdo <guilherme.abdo@codeblox.com.br>
2025-04-09 13:54:50 -0300

    ⭐ div removal in focusonrender and focusfirstfieldoninvalidsubmit

--- Kit/Core/Components/Focus/BdkFocusOnRender.razor.cs
+++ Kit/Core/Components/Focus/BdkFocusOnRender.razor.cs
@@ -26,7 +26,7 @@ public partial class BdkFocusOnRender : ComponentBase, IAsyncDisposable
                     Selector,
                     Delay
                 );
-
+            StateHasChanged();
         }
     }
 
