6c5b9a379169738f8035c89aea191e5315d518f7
Guilherme Meinlschmiedt Abdo <guilherme.abdo@codeblox.com.br>
2026-02-04 16:11:16 -0300

    ⭐ BdkLoader: option for load only on webassembly

---
+++ Kit/Core/Components/Loader/BdkLoaderMode.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorDevKit;
+
+public enum BdkLoaderMode
+{
+    /// <summary>
+    /// Load occurs during prerender and interactive rendering (default)
+    /// </summary>
+    Repeat = 0,
+    /// <summary>
+    /// Load during prerender and persist the loaded state for interactive rendering
+    /// </summary>  
+    Persist,
+    /// <summary>
+    /// Load only when running in WebAssembly (interactive rendering only)]
+    /// </summary>
+    WebAssemblyOnly,
+}
